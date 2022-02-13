305eaa259abfebe3dbf7916fdf5df5bbd70369ac
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-02-12 23:10:30 -0500

    [Code][Player] Add Look Input & events

--- Project Bunny/Assets/Scripts/Input/StudentController.cs
+++ Project Bunny/Assets/Scripts/Input/StudentController.cs
@@ -5,15 +5,59 @@ namespace Input
 {
     public class StudentController : MonoBehaviour
     {
+        [Header("Input")]
         [SerializeField] private CharacterController _characterController;
-        
-        private Vector3 _currentMovement;
-        
-        public void OnMove(InputValue input)
+        [SerializeField] private float _movementSpeed;
+        private Vector3 _currentPosition;
+
+        private void Awake()
         {
-            Vector2 inputVec = input.Get<Vector2>();
-            _currentMovement = new Vector3(inputVec.x, 0, inputVec.y);
-            _characterController.Move(_currentMovement);
+            if (_characterController == null)
+            {
+                _characterController = gameObject.GetComponent<CharacterController>();
+            }
         }
+
+        private void FixedUpdate()
+        {
+            MoveStudent();
+        }
+
+        #region InputSystem
+        
+        private void MoveStudent()
+        {
+            _characterController.Move(_currentPosition * (_movementSpeed * Time.deltaTime));
+        }
+
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            var inputMovement = value.ReadValue<Vector2>();
+            _currentPosition = new Vector3(inputMovement.x, 0f, inputMovement.y);
+        }
+
+        public void OnLook()
+        {
+            var mousePosition = Mouse.current.position.ReadValue();
+            var rotation = MousePosToRotationInput(mousePosition);
+            transform.rotation = Quaternion.Euler(0f, rotation, 0f);
+        }
+        
+        private float MousePosToRotationInput(Vector2 mousePos)
+        {
+            var target = transform;
+            if (Camera.main is { })
+            {
+                Vector3 objectPos = Camera.main.WorldToScreenPoint(target.position);
+
+                mousePos.x = mousePos.x - objectPos.x;
+                mousePos.y = mousePos.y - objectPos.y;
+            }
+
+            var angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            return 90 - angle;
+        }
+        
+        #endregion
     }
 }
