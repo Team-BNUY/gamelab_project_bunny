0a8a1996a1a030f1ee3aab6b35ec4e49b0bffb12
Kris <kristiankurk@gmail.com>
2022-02-19 10:58:30 -0500

    [Code] [Networking] Added namespaces
    i always forget that they're a thing

--- Project Bunny/Assets/Scripts/Networking/RoomManager.cs
+++ Project Bunny/Assets/Scripts/Networking/RoomManager.cs
@@ -3,34 +3,40 @@ using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 
-public class RoomManager : MonoBehaviourPunCallbacks
+namespace Networking
 {
-    public const string GAME_SCENE_NAME = "NetworkPlayground";
-
-    [SerializeField] private TMP_InputField createInput;
-    [SerializeField] private TMP_InputField joinInput;
-
-    private Button createButton;
-    private Button joinButton;
-
-    void Start()
+    public class RoomManager : MonoBehaviourPunCallbacks
     {
-        createButton = createInput.GetComponentInChildren<Button>();
-        joinButton = joinInput.GetComponentInChildren<Button>();
-        createButton.onClick.AddListener(CreateRoom);
-        joinButton.onClick.AddListener(JoinRoom);
+        public const string GAME_SCENE_NAME = "NetworkPlayground";
 
-    }
+        [SerializeField] private TMP_InputField createInput;
+        [SerializeField] private TMP_InputField joinInput;
 
-    public void CreateRoom() {
-        PhotonNetwork.CreateRoom(createInput.text);
-    }
+        private Button createButton;
+        private Button joinButton;
 
-    public void JoinRoom() {
-        PhotonNetwork.JoinRoom(joinInput.text);
-    }
+        void Start()
+        {
+            createButton = createInput.GetComponentInChildren<Button>();
+            joinButton = joinInput.GetComponentInChildren<Button>();
+            createButton.onClick.AddListener(CreateRoom);
+            joinButton.onClick.AddListener(JoinRoom);
 
-    public override void OnJoinedRoom() {
-        PhotonNetwork.LoadLevel(GAME_SCENE_NAME);
+        }
+
+        public void CreateRoom()
+        {
+            PhotonNetwork.CreateRoom(createInput.text);
+        }
+
+        public void JoinRoom()
+        {
+            PhotonNetwork.JoinRoom(joinInput.text);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            PhotonNetwork.LoadLevel(GAME_SCENE_NAME);
+        }
     }
 }
