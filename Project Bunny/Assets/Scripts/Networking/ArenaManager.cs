f0b12449087512efee10aa8d67cb371f3f0c417c
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-04-02 17:01:57 -0400

    [Code][BugFix] Modify ArenaManager.cs Start Method

--- Project Bunny/Assets/Scripts/Networking/ArenaManager.cs
+++ Project Bunny/Assets/Scripts/Networking/ArenaManager.cs
@@ -113,8 +113,6 @@ public class ArenaManager : MonoBehaviourPunCallbacks
 
         if (PhotonNetwork.IsMasterClient)
         {
-            _allPlayers = Array.Empty<NetworkStudentController>();
-            Invoke(nameof(GetAllPlayers), 0.1f);
             InitializeGiantRollballs();
         }
 
