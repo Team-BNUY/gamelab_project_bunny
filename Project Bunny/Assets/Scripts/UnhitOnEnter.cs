9088a0bac15bfa688d82c3c819ab1f2833c4eb1f
Julien Picard <julien.picard01@outlook.com>
2022-04-03 19:40:46 -0400

    [Code][Animation] Attempt to fix student hit animation again

--- Project Bunny/Assets/Scripts/UnhitOnEnter.cs
+++ Project Bunny/Assets/Scripts/UnhitOnEnter.cs
@@ -2,9 +2,9 @@ using AI.Agents;
 using Player;
 using UnityEngine;
 
-public class UnhitOnExit : StateMachineBehaviour
+public class UnhitOnEnter : StateMachineBehaviour
 {
-    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.GetComponent<Student>()?.Unhit();
         animator.GetComponent<Teacher>()?.Unhit();
