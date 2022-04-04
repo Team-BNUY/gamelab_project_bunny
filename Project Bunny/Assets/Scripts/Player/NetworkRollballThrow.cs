155bb5d73515d163c2b440e6104c96cbe341601a
George Mavroeidis <gdmavroeidis@hotmail.com>
2022-04-04 06:07:37 -0400

    [Code][Gameplay] Start Interactable Aiming Arrow Functionality

--- Project Bunny/Assets/Scripts/Player/NetworkRollballThrow.cs
+++ Project Bunny/Assets/Scripts/Player/NetworkRollballThrow.cs
@@ -53,7 +53,7 @@ namespace Player
         /// </summary>
         public void Enter(NetworkStudentController currentStudentController)
         {
-            if (_currentRollball == null || _currentStudentController.HasSnowball) return;
+            if (_currentRollball == null || currentStudentController.HasSnowball) return;
             
             //Initialize key variables
             _isActive = true;
