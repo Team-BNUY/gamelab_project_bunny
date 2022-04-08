f3800f753a54ce4af08a5c67b4810693c9405efc
Julien Picard <julien.picard01@outlook.com>
2022-04-07 20:28:10 -0400

    [Code][Sound] Sync school bell and make radio control music volume

--- Project Bunny/Assets/Scripts/AudioManager.cs
+++ Project Bunny/Assets/Scripts/AudioManager.cs
@@ -1,17 +1,25 @@
+using Photon.Pun;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManager : MonoBehaviourPunCallbacks
 {
     public static AudioManager Instance;
 
-    [SerializeField] private AudioClip[] woodenFootsteps;
+    [SerializeField] private AudioClip[] syncClips;
     
     private AudioSource _audioSource;
     private bool _muted;
+    private float _volume = 1.0f;
 
     public bool Muted
     {
         set => _muted = value;
+    }
+
+    public float Volume
+    {
+        get => _volume;
+        set => _volume = value;
     }
 
     private void Awake()
@@ -25,7 +33,7 @@ public class AudioManager : MonoBehaviour
     {
         if (_muted) return;
         
-        _audioSource.PlayOneShot(clip, volume);
+        _audioSource.PlayOneShot(clip, _volume * volume);
     }
 
     public void Play(AudioClip clip, float volume = 1f, bool loop = false)
@@ -33,7 +41,28 @@ public class AudioManager : MonoBehaviour
         if (_muted) return;
         
         _audioSource.clip = clip;
+        _audioSource.volume = _volume * volume;
         _audioSource.loop = loop;
         _audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        _audioSource.Stop();
+    }
+
+    public void PlaySync(int clipID, float volume)
+    {
+        if (clipID >= syncClips.Length) return;
+
+        photonView.RPC(nameof(PlaySyncRPC), RpcTarget.All, clipID, _volume * volume);
+    }
+
+    [PunRPC]
+    private void PlaySyncRPC(int clipID, int volume)
+    {
+        var clip = syncClips[clipID];
+        _audioSource.volume = _volume * volume;
+        PlayOneShot(clip, volume);
     }
 }
